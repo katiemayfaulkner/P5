6091efae9f2fa8b6c6f1165c7ae303392e8530b3
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-06-21 15:28:43 +0200

    refactor : Make js file content easier to read

--- js/cart.js
+++ js/cart.js
@@ -13,11 +13,10 @@ function ready() {
     for(var i = 0; i < removeBtn.length; i++) {
         var btn = removeBtn[i]
         btn.addEventListener('click', removeCart)
-            console.log('clicked')
-            
+            console.log('clicked')       
     }
 
- 
+
     // QUANTITY BUTTONS
     var quantityInputs = document.getElementsByClassName('itemQuantity')
     
@@ -29,86 +28,84 @@ function ready() {
 }
 
 // REMOVE BUTTON
-function removeCart(event) {
-    var btnClicked =  event.target
-    btnClicked.parentElement.remove()
-    updateTotal()
+    function removeCart(event) {
+        var btnClicked =  event.target
+        btnClicked.parentElement.remove()
+        updateTotal()
 
-}
+    }
 
 // QUANTITY BUTTONS
-function quantityChanged(event) {
-    var input = event.target  
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
+    function quantityChanged(event) {
+        var input = event.target  
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1
 
-    }
-    updateTotal()
-}
- 
-var quantityBtn = document.getElementById('Button')
-
-// append the buttons to the input
-$(function() {
-
-    $("inputBox").append('inputBtn');
-    
-});
-
-// make buttons work
-$(".button").on("click", function() {
-    
-    // Cache the selector
-    var $button = $(this);
-    // Save the “old” value of the input (value at time of clicking)
-    var oldValue = $button.parent().find("input").val();
-    
-    // If the “plus” button is clicked…
-    if ($button.text() == "+") {
-        // …increment value up one
-        var newVal = parseFloat(oldValue) + 1;
-        
-    } else { // Else if the “minus” button is clicked…
-
-        var newVal = parseFloat(oldValue) - 1;
-        // Don't allow decrementing below zero
-        if (oldValue > 0) {
-            // …decrement the value down one
-            var newVal = parseFloat(oldValue) - 1;
-        } else {
-            newVal = 0;
         }
+        updateTotal()
     }
-    
-  // Drop the new value into the input
-  $button.parent().find("input").val(newVal);
 
-  updateTotal()
+    var quantityBtn = document.getElementById('Button')
 
-});
+    // append the buttons to the input
+        $(function() {
 
-function updateTotal() {
-    
-    var cartContainer = document.getElementById('cartProducts')
-    var cartRows = cartContainer.getElementsByClassName('cartProduct')
-    var total = 0
-    
-    for(var i = 0; i < cartRows.length; i++) { 
-        var cartRow = cartRows[i] 
-        var priceElement = cartRow.getElementsByClassName('price')[0]
-        var quantityElement = cartRow.getElementsByClassName('itemQuantity')[0]
-        
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-        
-    }
-    
-    total = Math.round(total * 100) / 100
-    document.getElementById('totalPrice').innerText = '$' + total
-}
+            $("inputBox").append('inputBtn');
+            
+        });
 
+    // make buttons work
+        $(".button").on("click", function() {
+            
+            // Cache the selector
+            var $button = $(this);
+            // Save the “old” value of the input (value at time of clicking)
+            var oldValue = $button.parent().find("input").val();
+            
+            // If the “plus” button is clicked…
+            if ($button.text() == "+") {
+                // …increment value up one
+                var newVal = parseFloat(oldValue) + 1;
+                
+            } else { // Else if the “minus” button is clicked…
 
+                var newVal = parseFloat(oldValue) - 1;
+                // Don't allow decrementing below zero
+                if (oldValue > 0) {
+                    // …decrement the value down one
+                    var newVal = parseFloat(oldValue) - 1;
+                } else {
+                    newVal = 0;
+                }
+            }
+            
+        // Drop the new value into the input
+        $button.parent().find("input").val(newVal);
+
+        updateTotal()
+
+        });
+
+        function updateTotal() {
+            
+            var cartContainer = document.getElementById('cartProducts')
+            var cartRows = cartContainer.getElementsByClassName('cartProduct')
+            var total = 0
+            
+            for(var i = 0; i < cartRows.length; i++) { 
+                var cartRow = cartRows[i] 
+                var priceElement = cartRow.getElementsByClassName('price')[0]
+                var quantityElement = cartRow.getElementsByClassName('itemQuantity')[0]
+                
+                var price = parseFloat(priceElement.innerText.replace('$', ''))
+                var quantity = quantityElement.value
+                total = total + (price * quantity)
+                
+            }
+            
+            total = Math.round(total * 100) / 100
+            document.getElementById('totalPrice').innerText = '$' + total
+        }
 
 
 // // TEMPORARY CART CONTENT
