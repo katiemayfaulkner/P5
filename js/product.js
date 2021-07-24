edeace434cc459849bbfb0d9b0249051590cfade
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-07-24 20:36:12 +0200

    feat: Add to cart function using localStorage

--- js/product.js
+++ js/product.js
@@ -20,17 +20,17 @@
 // yahya.talk('BOM BOM BOM BOM.  ')
 
     
-// TODO: Get single product details from the API via product ID 
+// GET SINGLE PROD DETAILS FROM THE API VIA PROD ID 
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
 
-console.log(productId)
-console.log(urlParams)
+console.log(productId);
+console.log(urlParams);
 
-var img = document.getElementById('productImg')
-var camera = document.getElementById('productName')
-var price = document.getElementById('productPrice')
-var description = document.getElementById('productDescription')
+var img = document.getElementById('productImg');
+var camera = document.getElementById('productName');
+var price = document.getElementById('productPrice');
+var description = document.getElementById('productDescription');
 let productOptions = document.getElementById('product_options');
 
 function getProduct() {
@@ -43,7 +43,6 @@ function getProduct() {
             camera.textContent = `${data.name}`;
             price.textContent = `${data.price / 100}.00$`;
             description.textContent = `${data.description}`;
-            // options.textContent = `${data.lenses}`;
             
             for (let i = 0; i < data.lenses.length; i++) {
                 let option = document.createElement('option');
@@ -53,43 +52,88 @@ function getProduct() {
             }
         })
         .catch(err => console.log(err))
-}
+};
 getProduct();
 
-/*** Select Input Event */
+
+// SELECT INPUT EVENT (LENSES)
 productOptions.addEventListener('change', (e) => {
     // Here's the value, printed in the console, save it wherever you want ;) (localStorage)
     console.log(e.target.value)
-})
+});
     
 
+//SAVE PRODUCT TO LOCALSTORAGE
+var addBtn = document.getElementById('addToCart')
 
+addBtn.onclick = function addToLocalStorage() {
+   
+    //things to store
+    var name = document.getElementById('productName').textContent
+    var price = document.getElementById('productPrice').textContent
+    var img = document.getElementById('productImg').src
 
+    console.log(name, price, img)
 
-// LOCALSTORAGE TEST
-//storing text input
-    //get element
-    var inputBox = document.getElementById('textInput')
-    
+    var oldInput = localStorage.getItem(name, price, img);
+
     //If we already have value, retrieve it
-    if(localStorage.getItem('userInput')){
+    if(localStorage.getItem(name, price, img)){
         // and update input with old/existing user input
-        var oldInput = localStorage.getItem('userInput');
-        inputBox.value = oldInput;
-        console.log("retrieve existing local storage key")
-
+        addBtn.value = oldInput;
+        console.log("Item already added to cart!")
+        alert("Item already added to cart!")
+        
     } else{
         // or store user input in local storqge
-        localStorage.setItem('userInput', '');
-        console.log("register new local storage key")
-
+        localStorage.setItem(name, price, img);
+        console.log("Item added to cart!")
     }
     
-    //console log
-    console.log(inputBox)
-
+     
+    // // LOCALSTORAGE TEST
+    // //storing text input
+    // //get element
+    // var inputBox = document.getElementById('textInput') //input box
+    // var oldInput = localStorage.getItem('userInput');
+    
+    // //If we already have value, retrieve it
+    // if(localStorage.getItem('userInput')){
+    //     // and update input with old/existing user input
+    //     inputBox.value = oldInput;
+    //     console.log("retrieve existing local storage key")
+        
+    // } else{
+    //     // or store user input in local storqge
+    //     localStorage.setItem('userInput', '');
+    //     console.log("register new local storage key")
+    // }
+    
+    // //console log
+    // console.log(inputBox)
+    
     // inputBox.addEventListener("keyup", function() {
-    // localStorage.setItem('userInput', inputBox.value)
-    // console.log(inputBox.value)
+        // localStorage.setItem('userInput', inputBox.value)
+        // console.log(inputBox.value)
+        
+        // })
 
-    // })
+
+
+    // // Get previous storage, if any
+    // var storage = JSON.parse(localStorage.getItem("cart"));
+    // if(storage==null){
+    //     storage = [];
+    // }
+    
+    // // Create an object to store
+    // var data = getProduct();
+    
+    // var storage = JSON.parse(localStorage.getItem("cart"));
+    // var storage = 
+    // storage.push(data);
+    
+    // // Store it
+    // localStorage.setItem("cart",JSON.stringify(storage));
+};
+
