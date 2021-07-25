6459183d78a3568e40a095e4fab8e34df221c7e2
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-07-25 14:50:19 +0200

    feat: Injecting localStorage data in to cart

--- js/cart.js
+++ js/cart.js
@@ -1,35 +1,144 @@
+var cart = document.getElementById('cartProducts')
+
+for (let i = 0; i < localStorage.length; i++){
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        var image = JSON.parse(localStorage.getItem("img"))
+
+    //Create box
+    let box = document.createElement('div');
+    box.classList.add('cartProduct')
+
+    cart.appendChild(box);
+
+    //Image
+    var img = document.createElement('img');
+    img.classList.add('img');
+    img.setAttribute('id', 'image');
+    img.src = img;
+    box.appendChild(img);
+
+    //Title
+    var title = document.createElement('h3');
+    title.classList.add('title', 'col-3');
+    title.setAttribute('id', 'productTitle');
+    title.innerHTML = key;
+
+    box.appendChild(title);
+
+    //Quantity
+    var quantityContainer = document.createElement('div');
+    quantityContainer.classList.add('quantity', 'col-3');
+
+    box.appendChild(quantityContainer);
+
+    var quantity = document.createElement('input');
+    quantity.classList.add('itemQuantity');
+    quantity.setAttribute('type', 'number');
+    quantity.setAttribute('value', '1');
+    quantity.setAttribute('name', 'quantity');
+
+    quantityContainer.appendChild(quantity);
+
+    //Price
+    var price = document.createElement('h3');
+    price.classList.add('price', 'col-3');
+    price.setAttribute('id', 'productPrice');
+    price.innerHTML = value;
+
+    box.appendChild(price);
+
+    //Remove
+    let remove = document.createElement('button');
+    remove.classList.add('btn-remove');
+    remove.setAttribute('id', 'removeBtn');
+    remove.innerHTML = 'Remove';
+
+    //Append everything
+    box.appendChild(remove);
+
+    console.log(key, value);
+
+    updateTotal()
+}
+
+
+//localStorage.removeItem(key); --> TO CLEAR ONE THING IN LOCALSTORAGE
+//localStorage.clear(); --> TO CLEAR EVERYTHING IN LOCALSTORAGE
+
+
+// // TEMPORARY CART CONTENT
+// var cart = document.getElementById('cartProducts')
+// var imgContainer = document.getElementById('productImg')
+
+// fetch('http://localhost:3000/api/cameras')
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+    
+//     for(let i = 0; i < data.length; i++) {
+        
+//         // Image
+//         // let img = document.createElement('img');
+//         //   img.setAttribute('src', data[i].imageUrl)
+//         //   img.classList.add('img')
+//         //   imgContainer.appendChild(img)
+
+//         var img = document.getElementById('productImg')
+//         img.setAttribute('src', data[i].imageUrl)
+        
+//         //Price
+//         // let price = document.createElement('p');
+//         //   price.innerHTML = `${data[i].price / 100}.00$`;
+//         //   price.classList.add('price')
+//         //   cart.appendChild(price)
+
+//         var price = document.getElementById('productPrice')
+//         price.innerHTML = `${data[i].price / 100}.00$`;
+
+//         }
+//     })
+//     .catch(err => console.log(err))
+
+
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+
+    document.addEventListener('DOMContentLoaded', ready);
 
 } else {
-    ready()
-}
+    ready();
+};
 
 function ready() {
 
     //REMOVE BUTTON
-    var removeBtn = document.getElementsByClassName('btn-remove') 
+    var removeBtn = document.getElementsByClassName('btn-remove');
 
     for(var i = 0; i < removeBtn.length; i++) {
-        var btn = removeBtn[i]
-        btn.addEventListener('click', removeCart)
-
-    }
+        var btn = removeBtn[i];
+        btn.addEventListener('click', removeCart);
+    };
 
 
     //QUANTITY
-    var quantityInput = document.getElementsByClassName('itemQuantity')
+    var quantityInput = document.getElementsByClassName('itemQuantity');
 
     for(var i = 0; i < quantityInput.length; i++) {
-        var input = quantityInput[i]
-        input.addEventListener('change', quantityChanged)
-    }
-}
+        var input = quantityInput[i];
+        input.addEventListener('change', quantityChanged);
+    };
+};
 
 //REMOVE BUTTON
+
 function removeCart(event) {
     var btnClicked =  event.target
     btnClicked.parentElement.remove()
+
+    var key = JSON.parse(localStorage.getItem(key))
+    // var key = $(this).attr('key');
+    window.localStorage.removeItem('key')
+    
     updateTotal()
 }
 
@@ -65,36 +174,3 @@ function updateTotal() {
     document.getElementById('totalPrice').innerText = '$' + total
 }
 
-
-// // TEMPORARY CART CONTENT
-// var cart = document.getElementById('cartProducts')
-// var imgContainer = document.getElementById('productImg')
-
-// fetch('http://localhost:3000/api/cameras')
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data);
-    
-//     for(let i = 0; i < data.length; i++) {
-        
-//         // Image
-//         // let img = document.createElement('img');
-//         //   img.setAttribute('src', data[i].imageUrl)
-//         //   img.classList.add('img')
-//         //   imgContainer.appendChild(img)
-
-//         var img = document.getElementById('productImg')
-//         img.setAttribute('src', data[i].imageUrl)
-        
-//         //Price
-//         // let price = document.createElement('p');
-//         //   price.innerHTML = `${data[i].price / 100}.00$`;
-//         //   price.classList.add('price')
-//         //   cart.appendChild(price)
-
-//         var price = document.getElementById('productPrice')
-//         price.innerHTML = `${data[i].price / 100}.00$`;
-
-//         }
-//     })
-//     .catch(err => console.log(err))
